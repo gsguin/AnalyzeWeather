e77d0a7501e8d3e53b67526b8ca651250bb0f880
gsguin <gsguin@yahoo.com>
2018-08-16 01:27:47 -0500

    Initial Upload

---
+++ Weather/main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 15 12:08:50 2018
+
+@author: GG9323
+"""
+
+from weatherdata import *
+
+def main():
+    weatherData = WeatherData()
+    print (weatherData.data)
+
+if __name__ == "__main__":
+    main()
